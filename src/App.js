6f6589af5c95ad92023126eb20697b3c05dac4b5
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-11-07 20:38:52 -0500

    branch test

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+
+function App() {
+  let [searchTerm, setSearchTerm] = useState('')
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState('Search for Music!')
+
+  const API_URL = `https://itunes.apple.com/search?term=`
+
+  function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
+
+  useEffect(() => {
+    if (searchTerm) {
+      document.title=`${searchTerm} Music`
+      const fetchData = async () => {
+        const response = await fetch(API_URL + searchTerm)
+        const resData = await response.json()
+        if(resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
+      }
+      fetchData()
+  }
+  }, [searchTerm, API_URL])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    term = toTitleCase(term)
+    setSearchTerm(term)
+    return (<Redirect to="/" />)
+  }
+
+  return (
+    <div className="App">
+      {message}
+      <Router>
+        <Route exact path="/">
+          <SearchBar handleSearch={handleSearch} />
+          <Gallery data={data} />
+        </Route>
+        <Route path="/album/:id">
+          <AlbumView term={searchTerm} />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistView term={searchTerm} />
+        </Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.js
@@ -0,0 +1,18 @@
+import GalleryItem from './GalleryItem'
+
+const Gallery = (props) => {
+
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem key={index} item={item} />
+        )
+    })
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery
